98e30f8bc06193d28105701b0733323d6c3cdefe
emmanuel-santhosh <emmanuel_santhosh@yahoo.com>
2026-07-13 18:13:23 +0200

    Removed unused imports

--- backend/src/test/java/capstone/backend/controller/GhAuthControllerTest.java
+++ backend/src/test/java/capstone/backend/controller/GhAuthControllerTest.java
@@ -1,20 +1,14 @@
 package capstone.backend.controller;
 
-import capstone.backend.DailyInspiration;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
 
 @SpringBootTest
