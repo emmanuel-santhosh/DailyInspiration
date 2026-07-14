2237f7ea25131b2a8b5ba2bcb4387f37130f1a41
emmanuel-santhosh <emmanuel_santhosh@yahoo.com>
2026-07-14 15:18:09 +0200

    basic frontend for oauth2 login via github

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,19 @@ import './App.css'
 
 function App() {
 
+    function oauth2Github() {
+        const host =
+            window.location.host === "localhost:5173"
+                ? "http://localhost:8080"
+                : window.location.origin;
+        window.open(host + "/oauth2/authorization/github", "_self");
+    }
 
-  return (
-    <>
-    </>
-  )
+    return (
+        <>
+            <button onClick={oauth2Github}>Authenticate via GitHub</button>
+        </>
+    )
 }
 
 export default App
