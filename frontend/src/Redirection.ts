1fd22028704d25a222bec6b283a798fc0bbf68ae
emmanuel-santhosh <82584441+emmanuel-santhosh@users.noreply.github.com>
2026-07-16 15:15:51 +0200

    Deleted duplicate file

--- frontend/src/Redirection.ts
+++
@@ -1,5 +0,0 @@
-export const backendUrl:string = "http://localhost:8080";
-export const frontendUri:string = "localhost:5173";
-
-export const oauthGithubUri:string = "/oauth2/authorization/github";
-export const oauthGithubMeEndpoint:string = "api/auth/gh/me";
