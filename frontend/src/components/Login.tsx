87ec4efcfacb78cb8e80d4aa57e4f2716e70a946
emmanuel-santhosh <82584441+emmanuel-santhosh@users.noreply.github.com>
2026-07-16 14:14:11 +0200

    - OAuth2Github refactored to button for consistency in formatting - Necessary functions refactored as service - Reformatted code

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,17 +1,25 @@
-import OAuth2Github from "./OAuth2Github.tsx";
+import {useEffect} from "react";
+import oAuth2Github, {loadUser} from "../services/oAuth2Github.ts";
 
 type userProps = {
     setUser: (user: string | undefined | null) => void;
 }
 
 export default function Login(props: Readonly<userProps>) {
+
+    useEffect(() => {
+        loadUser(props);
+    }, []);
+
     return (
         <>
             <header>
                 <h1>Welcome !</h1>
             </header>
             <div className="pageContainerPreLogin">
-                <OAuth2Github setUser={props.setUser}></OAuth2Github>
+                <button className={"firstChild"}
+                        onClick={oAuth2Github}>Authenticate via GitHub
+                </button>
                 <button>Create account</button>
                 <button>Login with username and password</button>
                 <button>Continue as Guest</button>
