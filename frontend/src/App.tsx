3c80ce9b878bea476a0468536386fe63a5324c85
emmanuel-santhosh <82584441+emmanuel-santhosh@users.noreply.github.com>
2026-07-26 07:37:00 +0200

    Username is passed via context instead of via props

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import './styles/App.css'
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import BasePreLogin from "./components/preLogin/BasePreLogin.tsx";
 import BasePostLogin from "./components/postLogin/BasePostLogin.tsx";
 import type {userType} from "./types/User.ts";
 import {loadUser} from "./services/loadUser.ts";
+import {UserContext} from "./contexts/UserContext.ts";
 
 function App() {
 
-    const [user, setUser] = useState<userType>(undefined);
+    const userFromContext = useContext<userType>(UserContext);
+    const [user, setUser] = useState<userType>(userFromContext);
 
     // Set page title
     useEffect(() => {
@@ -23,12 +25,14 @@ function App() {
 
     return (
         <>
+            <UserContext value={user}>
             {typeof user != "string" &&
                 <BasePreLogin></BasePreLogin>
             }
             {typeof user === "string" &&
                 <BasePostLogin></BasePostLogin>
             }
+            </UserContext>
             <footer>
                 <small>© Emmanuel Santhosh 2026. All rights reserved.</small>
             </footer>
--- frontend/src/components/postLogin/StartPagePostLogin.tsx
+++ frontend/src/components/postLogin/StartPagePostLogin.tsx
@@ -1,7 +1,14 @@
+import {useContext} from "react";
+import {UserContext} from "../../contexts/UserContext.ts";
+import type {userType} from "../../types/User.ts";
+
 export default function StartPagePostLogin() {
+
+    const user:userType = useContext(UserContext);
+
     return(
         <>
-            <h1>Welcome, !</h1>
+            <h1>Welcome, {user}!</h1>
         </>
     )
 }
