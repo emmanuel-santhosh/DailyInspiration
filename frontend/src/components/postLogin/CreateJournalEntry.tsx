0054e7c3c97d2d09ac7d83880757fd0977bc011c
emmanuel-santhosh <82584441+emmanuel-santhosh@users.noreply.github.com>
2026-07-27 11:30:01 +0200

    Replaced csrf disable by spring defaults for single-page application

--- frontend/src/components/postLogin/CreateJournalEntry.tsx
+++ frontend/src/components/postLogin/CreateJournalEntry.tsx
@@ -1,5 +1,34 @@
 export default function CreateJournalEntry() {
-    return(
-        <></>
+    return (
+        <>
+            <header>
+                <h2>
+                    Jot down your thoughts
+                </h2>
+            </header>
+            <form>
+                <label htmlFor={"quote"}>Quote:</label>
+                <input id={"quote"}
+                       name={"quote"}
+                       type={"text"}
+                       required={true}
+                       maxLength={500}
+                       size={50}
+                />
+                <br/>
+                <label htmlFor={"topic"}>Topic:</label>
+                <input id={"topic"}
+                       name={"topic"}
+                       type={"text"}
+                       required={true}
+                       maxLength={50}
+                       size={20}
+                />
+                <br/>
+                <input className={"create__Journal__Entry"}
+                       type={"submit"}
+                value={"Create"}></input>
+            </form>
+        </>
     )
 }
