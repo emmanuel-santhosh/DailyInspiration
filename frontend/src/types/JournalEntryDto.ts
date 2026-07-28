f1b7876d0c3652fce22a8f6c91b025c3c943b5e1
emmanuel-santhosh <82584441+emmanuel-santhosh@users.noreply.github.com>
2026-07-28 17:42:43 +0200

    - Updated DTOs in frontend

---
+++ frontend/src/types/JournalEntryDto.ts
@@ -0,0 +1,13 @@
+export type JournalEntryRequestDto = {
+    quote:string,
+    topic:string
+}
+
+export const MAX_LENGTH_QUOTE:number = 500;
+export const MAX_LENGTH_TOPIC:number = 50;
+
+export const BASE_BACKEND_URI:string = "/api/myjournal";
+
+export interface JournalEntryResponseDto extends JournalEntryRequestDto{
+    id:number
+}
