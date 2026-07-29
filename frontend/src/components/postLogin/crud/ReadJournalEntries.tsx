4070a1d7d16a773d8140a756b46e8f0436067d83
emmanuel-santhosh <82584441+emmanuel-santhosh@users.noreply.github.com>
2026-07-29 13:42:02 +0200

    - Removed duplicate Context - Updated dependency array in useEffect

--- frontend/src/components/postLogin/crud/ReadJournalEntries.tsx
+++ frontend/src/components/postLogin/crud/ReadJournalEntries.tsx
@@ -8,7 +8,7 @@ export default function ReadJournalEntries() {
 
     useEffect(() => {
         void fetchJournalEntries({setJournalEntries,setLoading});
-    }, [journalEntries]);
+    }, []);
 
     if (loading) {
         return (
--- frontend/src/context/UserContext.ts
+++
@@ -1,4 +0,0 @@
-import {createContext} from "react";
-import type {userType} from "../types/User.ts";
-
-export const UserContext = createContext<userType>(undefined);
