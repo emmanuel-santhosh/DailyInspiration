979af27ccfdb4b2e07e0ef25b45574be6ceae6ec
emmanuel-santhosh <82584441+emmanuel-santhosh@users.noreply.github.com>
2026-07-29 20:55:37 +0200

    Updating done via modal element

--- frontend/src/components/postLogin/crud/ListOfJournalEntries.tsx
+++ frontend/src/components/postLogin/crud/ListOfJournalEntries.tsx
@@ -1,5 +1,7 @@
-import type {JournalEntryResponseDto} from "../../../types/JournalEntryDto.ts";
+import type {JournalEntryRequestDto, JournalEntryResponseDto} from "../../../types/JournalEntryDto.ts";
 import type {JournalEntryOperation} from "../../../types/JournalEntryOperation.ts";
+import {useState} from "react";
+import Modal from "./Modal.tsx";
 
 interface ListOfJournalEntriesProps {
     journalEntries: JournalEntryResponseDto[];
@@ -7,24 +9,47 @@ interface ListOfJournalEntriesProps {
 }
 
 export default function ListOfJournalEntries(props: ListOfJournalEntriesProps) {
+
+    // Following hooks are for modal component
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [selectedJournalEntry, setSelectedJournalEntry] = useState<JournalEntryRequestDto>({quote: "", topic: ""});
+    const [id, setId] = useState<number>(1);
+
+    const clickHandler = (journalEntry: JournalEntryRequestDto) => {
+        setIsOpen(true);
+        setSelectedJournalEntry(journalEntry);
+    };
+
     return (
         <>
             <section className={"journal__entry__section"}>
                 {
                     props.journalEntries.map((journalEntry) =>
-                        /*
+                        <>
+                            {/*
                         * https://react.dev/learn/rendering-lists
-                        * */
-                        <article className={"journal__entry__holder"}
-                                 key={journalEntry.id}
-                                 id={`journal__entry__${journalEntry.id}`}>
-                            <p> Quote: {journalEntry.quote}</p>
-                            <br/>
-                            <p> Topic: {journalEntry.topic}</p>
-                            <br/>
-                        </article>
+                        * */}
+                            <article className={"journal__entry__holder"}
+                                     key={journalEntry.id}
+                                     id={`journal__entry__${journalEntry.id}`}>
+                                <p> Quote: {journalEntry.quote}</p>
+                                <br/>
+                                <p> Topic: {journalEntry.topic}</p>
+                                <br/>
+                            </article>
+                            {(props.operation !== "Create" && props.operation !== "Read") &&
+                                <button key={`button_${journalEntry.id}`}
+                                        onClick={() => {
+                                            clickHandler(journalEntry);
+                                            setId(journalEntry.id);
+                                        }}>{props.operation}</button>}
+                        </>
                     )
                 }
+                {isOpen && <Modal journalEntry={selectedJournalEntry}
+                                  setIsOpen={setIsOpen}
+                                  operation={props.operation}
+                                  id={id}/>}
             </section>
         </>
     )
