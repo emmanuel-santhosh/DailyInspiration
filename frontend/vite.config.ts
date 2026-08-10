9d7870e778c05f7c98d6a91c1cb2ea340b9c206e
emmanuel-santhosh <82584441+emmanuel-santhosh@users.noreply.github.com>
2026-08-10 18:40:08 +0200

    Installed Tailwind CSS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,18 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: "http://localhost:8080",
-        changeOrigin: true
-      }
+    plugins: [
+        tailwindcss(),
+        react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: "http://localhost:8080",
+                changeOrigin: true
+            }
+        }
     }
-  }
 })
